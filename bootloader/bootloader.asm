7c3f7d3c8a6096a6e94b418189a1aabee43445e3
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-03-30 23:02:43 +0300

    Добавил приветствие для подписчиков

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,1 +1,18 @@
-[org 0x7c00]
+[org 0x7c00]              ; относительно этого адреса 
+; bits 16                   ; сообщаем NASM в каком режиме будем работать
+
+    mov bx, HELLO
+    call print
+
+    call print_new_line
+
+    jmp $
+
+%include "bootloader/print16.asm"
+
+HELLO:
+    db 'Welcome, my brothers!!!', 0
+
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
