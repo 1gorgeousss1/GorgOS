490a03ee62c853f9b6bb605217dd0cd9b7dd4176
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-03-31 23:59:37 +0300

    загрузчик теперь считывает 64 сектора с внешнего устройства

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,18 +1,30 @@
-[org 0x7c00]              ; относительно этого адреса 
-; bits 16                   ; сообщаем NASM в каком режиме будем работать
+[org 0x7C00]              ; относительно этого адреса 
+bits 16                   ; сообщаем NASM в каком режиме будем работать
+
+
+    mov bp, 0x80000        ; настройка стека
+    mov sp, bp
 
     mov bx, HELLO
-    call print
+    call print16
 
     call print_new_line
 
+    mov dh, 64
+    call load_sectors
+
+    mov bx, SUCCESS
+    call print16
     jmp $
 
 %include "bootloader/print16.asm"
+%include "bootloader/load_sectors.asm"
 
 HELLO:
     db 'Welcome, my brothers!!!', 0
 
+SUCCESS:
+    db 'SUCCESS', 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
