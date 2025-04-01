8b1e99928ac5b91769399f92fef53dfad242317e
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-04-02 00:03:29 +0300

    добавлен вызов функции, которая переводит в 32 битный режим

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -2,29 +2,34 @@
 bits 16                   ; сообщаем NASM в каком режиме будем работать
 
 
-    mov bp, 0x80000        ; настройка стека
-    mov sp, bp
 
     mov bx, HELLO
     call print16
 
     call print_new_line
 
+    mov bx, KERNEL        ; из этого регистра будет браться, в который загрузится ядро
     mov dh, 64
     call load_sectors
 
     mov bx, SUCCESS
     call print16
-    jmp $
+
+    call switch_to_pm
+
 
 %include "bootloader/print16.asm"
 %include "bootloader/load_sectors.asm"
+%include "bootloader/switch_to_pm.asm"
+%include "bootloader/gdt.asm"
 
 HELLO:
     db 'Welcome, my brothers!!!', 0
 
 SUCCESS:
     db 'SUCCESS', 0
+
+KERNEL equ 0x1000
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
