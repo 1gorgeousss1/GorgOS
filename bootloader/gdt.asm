ddd2f8cb6f9836de7eb3b6f8a2a248043e04a4f4
1gorgeousss1 <akhilgov_magomed@inbox.ru>
2025-04-02 23:31:55 +0300

    повысил читаемость кода

--- bootloader/gdt.asm
+++ bootloader/gdt.asm
@@ -3,22 +3,21 @@ gdt_start:
 
 gdt_code:
     dw 0xFFFF
-    dw 0x0
-    db 0x0
+    dw 0x0000
+    db 0x00
     db 10011010b
     db 11001111b
-    db 0x0
+    db 0x00
 
 gdt_data:
     dw 0xFFFF
-    dw 0x0
-    db 0x0
+    dw 0x0000
+    db 0x00
     db 10010010b
     db 11001111b
-    db 0x0
+    db 0x00
 
 gdt_end:
-
 
 gdt_ptr:
     dw gdt_end - gdt_start - 1
